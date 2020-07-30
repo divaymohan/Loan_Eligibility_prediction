254262ae1b461bc78d2be938c61de52d56ec5664
Divay Mohan <divaymohan05@gmail.com>
2020-07-30 14:44:56 +0530

    adding label encoder to the lib

--- src/encoder.py
+++ src/encoder.py
@@ -1,10 +1,28 @@
 import pandas as pd
 import numpy as np
+import sklearn import preprocessing
 
 def OneHotEncoder_preprocessor(data,columns):
     return data
 
 def LabelEncoder_preprocessor(data,columns):
+    """LabelEncoder_preprocessor function take data and target columns list as perameter
+
+    Args:
+        data (pandas.DataFrame): DataFrame of data
+        columns (list): list of target columns 
+
+    Returns:
+        DataFrame: data with replaced column with encodings
+    """
+    for column in columns:
+        # fill na with new category as none
+        data.loc[:,column] = data[column].fillna('None')
+        # initialize LabelEncoder
+        lbl_enc = preprocessing.LabelEncoder()
+        # fit label encoder and transform values on column
+        # P.S do not use this directly .fit first, then transform
+        df.loc[:,column] = lbl_enc.fit_transform(data[column].values)
     return data
 
 def BinarizeEncoder_preprocessor(data,columns):
