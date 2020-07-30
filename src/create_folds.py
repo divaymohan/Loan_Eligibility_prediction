eeba418c2f679318c132018dfefe26b505d1d382
Divay Mohan <divaymohan05@gmail.com>
2020-07-30 14:15:01 +0530

    adding kfold again

--- src/create_folds.py
+++ src/create_folds.py
@@ -5,7 +5,7 @@ from sklearn import model_selection
 # this function can be used for regression problem
 def create_folds(data):
     # we create a new column called kfold and fill it with -1
-    data["kfold"] = -1
+    data["kfolds"] = -1
 
     # the next step is to randomize the rows of the data
     data = data.sample(frac=1).reset_index(drop=True)
@@ -32,25 +32,25 @@ def create_folds(data):
     return data
 
 
-if __name__ == "__main__":
+#if __name__ == "__main__":
     # Training data is in a csv file called train.csv
-    df = pd.read_csv("train.csv")
+    #df = pd.read_csv("train.csv")
 
     # we create a new columns called kfolds and fill it with -1
-    df["kfold"] = -1
+    #df["kfold"] = -1
 
     # the next step is to randomize the rows of the data
-    df = df.sample(frac=1).reset_index(drop=True)
+    #df = df.sample(frac=1).reset_index(drop=True)
 
     # initiate the kfold class from model_selection module
-    kf = model_selection.StratifiedKFold(n_splits=5)
+    #kf = model_selection.StratifiedKFold(n_splits=5)
 
     #kf = model_selection.kFold(n_splits=5)
 
     # fill the new kfold column
-    for fold, (trn_, val_) in enumerate(kf.split(X=df)):
-        df.loc[val_, "Kfold"] = fold
+    #for fold, (trn_, val_) in enumerate(kf.split(X=df)):
+    #    df.loc[val_, "Kfold"] = fold
 
     # save the new csv with kfold column
-    df.to_csv("train_folds.csv", index=False)
+   # df.to_csv("train_folds.csv", index=False)
 
